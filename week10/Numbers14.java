b6309936de06a42cc98e094b93a175f4e4a38c60
Kevv-thon <kevinbramastaa@gmail.com>
2023-11-10 13:47:48 +0700

    feat: added more feature to Numbers14.java

--- week10/Numbers14.java
+++ week10/Numbers14.java
@@ -1,10 +1,21 @@
 package week10;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Numbers14 {
     public static void main(String[] args) {
-        int[][] myNumbers = new int[3][];
+        int[][] myNumbers = new int[3][5];
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Length of row " + (i + 1) + ": " + myNumbers[i].length);
+        }
     }
 }
