bdde032ad28bf5ce1abdd9d7880356350ba67e06
Kevv-thon <kevinbramastaa@gmail.com>
2023-11-12 19:47:15 +0700

    feat: added calculation for average studen who pass and who failed

--- week9/arrayAverageScore14.java
+++ week9/arrayAverageScore14.java
@@ -7,19 +7,40 @@ public class arrayAverageScore14 {
         Scanner sc = new Scanner(System.in);
 
         int[] scores = new int[10];
-        double total = 0;
-        double average;
+        double total = 0, counterPasses = 0, counterFails = 0, totalPasses = 0, totalFails = 0;
+        double average, averagePasses = 0, averageFails = 0;
 
-        for (int i = 0; i < scores.length; i++) {
-            System.out.println("Enter student score " + (i + 1) + ": ");
+        System.out.print("Enter the number of students: ");
+        int students = sc.nextInt();
+
+        for (int i = 0; i < students; i++) {
+            System.out.print("Enter student score " + (i + 1) + ": ");
             scores[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > 70) {
+                counterPasses++;
+            } else {
+                counterFails++;
+            }
             total += scores[i];
         }
 
-        average = total / scores.length;
+        for (int i = 0; i < students; i++) {
+            if (scores[i] > 70) {
+                totalPasses += scores[i];
+                averagePasses = totalPasses / counterPasses;
+            } else {
+                totalFails += scores[i];
+                averageFails = totalFails / counterFails;
+            }
+        }
+
+        average = total / students;
+        System.out.println("The number of student who passes is " + counterPasses);
         System.out.println("The class average score is " + average);
+
+        System.out.println();
+
+        System.out.println("The average score of student who passes is " + averagePasses);
+        System.out.println("The average score of student who fails is " + averageFails);
     }
 }
