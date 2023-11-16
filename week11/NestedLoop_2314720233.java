0bf891b067cd7d328154f89a48de993f785a041d
Kevv-thon <kevinbramastaa@gmail.com>
2023-11-17 00:31:48 +0700

    feat: add this file

---
+++ week11/NestedLoop_2314720233.java
@@ -0,0 +1,28 @@
+package week11;
+
+import java.util.Scanner;
+
+public class NestedLoop_2314720233 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[][] temps = new double[5][7];
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[i].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = input.nextDouble();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[i].length; j++) {
+                System.out.println(temps[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
