2fc93bddf810eca0353cd6ebd758952a19157178
Kevv-thon <kevinbramastaa@gmail.com>
2023-11-17 00:22:30 +0700

    fix: updated week 11

--- week11/Triangle.java
+++ week11/Triangle.java
@@ -2,17 +2,19 @@ package week11;
 
 import java.util.Scanner;
 
-public class Square {
+public class Triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-
-        for (int iOuter = 1; iOuter <= N; iOuter++) {
-            for (int i = 1; i <= N; i++) {
+        int i = 0;
+        while (i <= N) {
+            int j = 0;
+            while (j < i) {
                 System.out.print("*");
+                j++;
             }
+            i++;
             System.out.println();
         }
     }
